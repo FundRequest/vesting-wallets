4987d4ccfa1ed26ed97795bc2ae1fa25309bb7c1
davyvanroy <davy.van.roy@gmail.com>
2018-03-20 13:32:17 +0100

    format vested balance

--- scripts/tokens-to-wallet.js
+++ scripts/tokens-to-wallet.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 const Web3 = require('web3');
 
 const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('https://ropsten.fundrequest.io'));
+web3.setProvider(new web3.providers.HttpProvider('https://mainnet.fundrequest.io'));
 web3.eth.getTransactionReceiptMined = require("./getTransactionReceiptMined.js");
 
 const p_key = process.env.PRIVATE_KEY;
@@ -27,13 +27,13 @@ const doThings = function (address, depositor, start, cliff, end, amount, _callb
   ];
   let payloadData = solidityFunction.toPayload(payload).data;
 
-  web3.eth.getTransactionCount('0xc31Eb6E317054A79bb5E442D686CB9b225670c1D', function (_, nonce) {
+  web3.eth.getTransactionCount('0x3ad396DcB86d3a72855f7E1e305EaA144ec9b434', function (_, nonce) {
     web3.eth.sendRawTransaction(sign({
-      to: '0x3d0bdd62f1b7636ac28e9dd9c6580b68dd652884',
+      to: '0x73B29c2a2Dd1c18Fe95cc43F67E5D202651794fE',
       value: 0,
-      gas: 500000,
+      gas: 300000,
       data: payloadData,
-      gasPrice: 8000000000,
+      gasPrice: 4000000000,
       nonce: nonce
     }, p_key), function (_, txHash) {
       if (_) {
@@ -54,18 +54,18 @@ const doThings = function (address, depositor, start, cliff, end, amount, _callb
 let iterate = function (lines) {
   let line = lines.pop();
   let schedules = line.split(",");
-  if (schedules.length === 5) {
+  if (schedules.length === 2) {
     let address = schedules[0];
-    let start = schedules[1];
-    let cliff = schedules[2];
-    let end = schedules[3];
-    let amount = schedules[4];
-    let depositor = '0xc31Eb6E317054A79bb5E442D686CB9b225670c1D';
+    let amount = schedules[1];
+    let start = 1518566400;
+    let cliff = 1534118400;
+    let end = 1534118400;
+    let depositor = '0x004F31991a12b24b4e69A33874936b5c94E2742D';
 
     let weiAmount = amount * Math.pow(10, 18);
     console.log('vesting tokens for ' + address + ' -> ' + weiAmount);
 
-    doThings(address, depositor, start, cliff, end, amount, function (_callback) {
+    doThings(address, depositor, start, cliff, end, weiAmount, function (_callback) {
       console.log("Done for address: " + address);
       if (lines.length > 0) {
         iterate(lines);
